d70b09488926783f5e1ef5af821959362042d250
PingHuangCheng <cheng553490@gmail.com>
2021-07-28 22:38:03 +0800

    final version

--- guessnum_46.py
+++ guessnum_46.py
@@ -3,9 +3,12 @@
 二、讓使用者重複輸入數字去猜
 三、猜對的話印出"終於猜對了！"
 四、猜錯的話，要告訴他比答案大/小
+五、延伸：讓使用者決定隨機數範圍
 '''
 import random
-r = random.randint(1,100)
+start = int(input('請決定隨機數字開始值： '))
+end = int(input('請決定隨機數字結束值: '))
+r = random.randint(start,end)
 count = 0
 while True:
     num = int(input('請輸入一個數字來猜: '))
